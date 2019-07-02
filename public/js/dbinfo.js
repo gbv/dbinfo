6c5588e69407b075c24ef47cedbbccbf69114ba9
Jakob Voss <voss@gbv.de>
2019-07-02 14:08:35 +0200

    Replace AngularJS with jQuery

--- public/js/dbinfo.js
+++ public/js/dbinfo.js
@@ -1,1 +1,19 @@
-var dbinfoApp = angular.module('dbinfoApp',['ngSuggest','ui.bootstrap','ui.bootstrap.typeahead']);
+$(function() {
+  $("#suggest-dbkey").easyAutocomplete({
+    url: function (searchTerms) {
+      return BASE + "/api/dbkey?id=" + encodeURI(searchTerms)
+    },
+    getValue: "completion",
+    template: {
+	  type: "description",
+	  fields: {
+		description: "description"
+	  }
+	}      
+  })
+
+  $("#suggest-form").submit(function(event) {
+    var url = BASE + "/" + $("#suggest-dbkey").val()
+    window.location.href = url
+  })
+})
